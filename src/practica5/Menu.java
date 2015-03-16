0be64475c423f2b805eb50529a6ad0d5e0d42324
Luis Beltran <luis.alberto.beltran.anaya@uabc.edu.mx>
2015-03-16 01:54:28 -0700

    Se Agrego la funcion que evalua si el tablero tiene solucion

--- src/practica5/Menu.java
+++ src/practica5/Menu.java
@@ -14,16 +14,49 @@ import java.util.*;
 public class Menu {
     
     public Menu(){
-        int[][] problema={{1,2,0},
-                          {4,5,3},
-                          {7,8,6}};
-        Nodo nodoProblema=new Nodo();
-        nodoProblema.estado=problema;
-        Puzzle puzzle=new Puzzle(nodoProblema);
-        
-        
+        int[][] problema={  {1,2,3},
+                            {4,5,6},
+                            {7,0,8}};
+        if(Evaluacion(problema)){
+            Nodo nodoProblema=new Nodo();
+            nodoProblema.estado=problema;
+            Puzzle puzzle=new Puzzle(nodoProblema);            
+        }
+        else{
+            System.out.println("Sistema no Tiene Solucion");
+        }
+
    
+    }
+    protected boolean Evaluacion(int [][] problema){
+        int[] vectorAux = new int[9];
+        int bandera = 0;
+        int contador=0;
         
+        for (int i = 0; i < problema.length; i++) {
+            for (int j = 0; j < problema[i].length; j++) {
+                if (problema[i][j]==0) {
+                    bandera=i;
+                }
+    
+                vectorAux[contador]=problema[i][j];
+                contador++;        
+            }
+        }
+        
+        contador=0;
+        
+        for (int i = 0; i < vectorAux.length; i++) {
+            for (int j = i+1; j < vectorAux.length; j++) {
+                if(vectorAux[i]>vectorAux[j]&&vectorAux[i]!=0&&vectorAux[j]!=0)
+                    contador++;
+            }
+        }
+        if((contador%2==0)&& (bandera%2==0) )
+            return true;
+        return false;
+
+            
     }
     
 }
